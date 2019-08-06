cdd69c0866a75fe84edbb48a4db65eeef776957f
FBoudet <florian.boudet@gmail.com>
2019-08-06 18:00:26 +0200

    Initialisation Daven ^^

--- test.py
+++ test.py
@@ -8,12 +8,12 @@ E = []
 n_test = 10000
 
 for i in range(n_test):
-    C1 = Combattant(30,{'PER':8,'INT':7,'CombatDistance':7},False,'Test_Distance',2,'Fusil',4,0,0,False)
+    Reiz = Combattant(30,{'PER':8,'INT':7,'CombatDistance':7},False,'Test_Distance',2,'Fusil',4,0,0,False)
     C2 = Combattant(65,{'FOR':7,'PER':5,'INT':4,'CombatMainsNues':7,'CombatContact':5,'CombatDistance':3},True,('Lame Défensive','Base Distance'),(5,0),('Hallebarde','Arc Long'),2)
-    C3 = Combattant(30,{'PER':8,'INT':7,'CombatDistance':7},False,'Test_Distance',2,'Fouet',4,0,0,False)
+    Daven = Combattant(30,{'FOR':4,'INT':3,'CombatMainsNues':4},False,'Arts Martiaux',1,None,4,0,0,False)
     C4 = getPNJ('Géant des Montagnes')
 
-    E.append(RandCombat([C1],[C4],3))
+    E.append(RandCombat([Reiz],[C4],3))
 
 result = np.array(E)
 g1=np.sum(result=='Groupe1')
